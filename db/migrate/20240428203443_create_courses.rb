7e176781bd6012eaaa615a81b68cd620c4c0d978
joel's vm <coons1@kenyon.edu>
2024-04-28 20:50:51 +0000

    Created Course model with database migration

---
+++ db/migrate/20240428203443_create_courses.rb
@@ -0,0 +1,13 @@
+class CreateCourses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
