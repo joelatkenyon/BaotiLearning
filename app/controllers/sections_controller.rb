3e0cf07ad21ab645d0eab1a3dd8a17883c01423f
joel's vm <coons1@kenyon.edu>
2024-05-07 00:26:26 +0000

    Hid enroll when instructor

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,6 @@
 class SectionsController < ApplicationController
+  before_action :require_instructor!
+
   def create
     @course = Course.find(params[:course_id])
     @section = @course.sections.create(section_params)
@@ -8,5 +10,13 @@ class SectionsController < ApplicationController
   private
     def section_params
       params.require(:section).permit(:title)
+    end
+
+    def require_instructor!
+        @course = Course.find(params[:course_id])
+        unless @course.check_user_role(current_user) == "instructor"
+            flash[:error] = "You must be an instructor of this course to continue."
+            redirect_to course_path(@course)
+        end
     end
 end
