4daef5ac0360c6371971d1c4ace92abd870ac390
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-05-07 13:15:43 -0400

    Update payments_controller.rb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,22 @@
-
-class PaymentsController < ApplicationController
-  before_action :authenticate_user! # Ensure user is authenticated before making a purchase
-
-  def purchase_course
-    course = Course.find(params[:course_id])
-    amount = (course.price * 100).to_i # Convert price to cents
-
-    begin
-      charge = Stripe::Charge.create({
-        amount: amount,
-        currency: 'usd',
-        source: params[:stripeToken],
-        description: "Purchase of #{course.name}"
-      })
-
-      # Update user's balance
-      current_user.balance -= course.price
-      current_user.save
-
-      # Handle successful purchase
-      flash[:success] = "Course purchased successfully!"
-      redirect_to courses_path
+Class PaymentsController < ApplicationController
+  def new
+  end
+  
+  def create
+    customer = Stripe::Customer.create({
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken]
+    })
+    
+    charge = Stripe::Charge.create({
+      :customer => customer.id,
+      :amount => 0,
+      :description => 'Description of your product',
+      :currency => 'usd'
+    })
+  
     rescue Stripe::CardError => e
-      # Handle card errors
       flash[:error] = e.message
-      redirect_to courses_path
-    end
+      redirect_to new_payment_path
   end
 end
