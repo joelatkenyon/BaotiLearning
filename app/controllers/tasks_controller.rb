45ffc30a4f42d1877c202976ddf9cd3fff57c0aa
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-05-08 01:08:43 -0400

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,28 +42,3 @@ class TasksController < ApplicationController
     end
 end
 
-according to this:
-
-class SectionsController < ApplicationController
-  before_action :require_instructor!
-
-  def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to course_path(@course)
-  end
-
-  private
-    def section_params
-      params.require(:section).permit(:title)
-    end
-
-    def require_instructor!
-        @course = Course.find(params[:course_id])
-        unless @course.check_user_role(current_user) == "instructor"
-            flash[:error] = "You must be an instructor of this course to continue."
-            redirect_to course_path(@course)
-        end
-    end
-end
-
