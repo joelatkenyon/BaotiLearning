d76929bf007498be255019d241d6893b4f5e7178
joel's vm <coons1@kenyon.edu>
2024-05-09 17:05:12 +0000

    Readded comparison for course start date

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,6 +3,6 @@ class Assignment < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :opendate, presence: true
+  validates :opendate, presence: true, comparison: {greater_than: DateTime.current}
   validates :closedate, presence: true, comparison: {greater_than: :opendate}
 end
